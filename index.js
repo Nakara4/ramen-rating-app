622b2bbef2e3284e393dc0308c72478edc4471f5
Dennis Karanja <ngangapepe8@gmail.com>
2025-03-17 11:01:53 +0300

    added ramen images

---
+++ index.js
@@ -0,0 +1,7 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "./images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "kojiro Ramen", restaurant: "Menya", image: "./images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "naruto Ramen", restaurant: "Ramen-ya", image: "./images/naruto.jpg" },
+    { id: 4, name: "nirvana Ramen", restaurant: "Ramen-ya", image: "./images/nirvana.jpg" },
+    { id: 5, name: "gyukotsyu Ramen", restaurant: "Ramen-ya", image: "./images/gyukotsyu.jpg" },
+ ];
