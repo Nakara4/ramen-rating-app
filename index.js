ee41d70316296765ef7a2f9c609f308735d6e5b0
Dennis Karanja <ngangapepe8@gmail.com>
2025-03-17 12:48:43 +0300

    added handleClick fuction to display ramen details

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "./images/shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "kojiro Ramen", restaurant: "Menya", image: "./images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "naruto Ramen", restaurant: "Ramen-ya", image: "./images/naruto.jpg" },
-    { id: 4, name: "nirvana Ramen", restaurant: "Ramen-ya", image: "./images/nirvana.jpg" },
-    { id: 5, name: "gyukotsyu Ramen", restaurant: "Ramen-ya", image: "./images/gyukotsyu.jpg" },
- ];
- const ramens = [
-   { src: './Images/gyukotsu.jpg', alt: 'gyukotsu ramen' },
-   { src: './Images/kojiro.jpg', alt: 'kojiro ramen' },
-   { src: './Images/naruto.jpg', alt: 'naruto ramen' },
-   { src: './Images/nirvana.jpg', alt: 'nirvana ramen' },
-   { src: './Images/shoyu.jpg', alt: 'shoyu ramen' }
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "./Images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Kojiro Ramen", restaurant: "Menya", image: "./Images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Naruto Ramen", restaurant: "Ramen-ya", image: "./Images/naruto.jpg" },
+    { id: 4, name: "Nirvana Ramen", restaurant: "Ramen-ya", image: "./Images/nirvana.jpg" },
+    { id: 5, name: "Gyukotsyu Ramen", restaurant: "Ramen-ya", image: "./Images/gyukotsu.jpg" },
 ];
 
 const ramenMenu = document.getElementById('ramen-menu');
+const ramenDetail = document.getElementById('ramen-detail');
 
 ramens.forEach(ramen => {
-   const img = document.createElement('img');
-   img.src = ramen.src;
-   img.alt = ramen.alt;
-   ramenMenu.appendChild(img);
+    const img = document.createElement('img');
+    img.src = ramen.image;
+    img.alt = ramen.name;
+    img.style.width = '100px';
+    img.style.height = '100px';
+    img.addEventListener('click', () => handleClick(ramen));
+    ramenMenu.appendChild(img);
 });
+
+function handleClick(ramen) {
+    ramenDetail.innerHTML = `
+        <h2>${ramen.name}</h2>
+        <p>Restaurant: ${ramen.restaurant}</p>
+        ${ramen.rating ? `<p>Rating: ${ramen.rating}</p>` : ''}
+        ${ramen.comment ? `<p>Comment: ${ramen.comment}</p>` : ''}
+    `;
+}
